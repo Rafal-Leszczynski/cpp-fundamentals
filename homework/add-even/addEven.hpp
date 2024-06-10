bafc26242e42e9adbcbcd4dc33ef59a2ec3d28c4
Rafał Leszczyński <85461555+Rafal-Leszczynski@users.noreply.github.com>
2024-06-10 03:48:07 +0200

    Update addEven.hpp

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -4,13 +4,13 @@
 int addEven(const std::vector<int> &numbers) {
   int answer = 0;
   int test = 0;
-  if (numbers.size() != 0) {
-  for (int i = 0; i <= numbers.size(); i++) {
+  //if (numbers.size() != 0) {
+  for (int i = 0; i < numbers.size(); i++) {
     test = numbers[i];
     if (test % 2 == 0) {
       answer = (test + answer);
     }
   }
-  }
+  //}
   return answer;
 }
