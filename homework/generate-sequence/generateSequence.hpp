1311e2b6e54b967dc2e3dabc81ccf53d685f6be3
Rafał Leszczyński <85461555+Rafal-Leszczynski@users.noreply.github.com>
2024-06-14 00:24:42 +0200

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -6,8 +6,8 @@ std::vector<int> generateSequence(int count, int step) {
     int meter = 0;
     if (count >0) {
         for (int i = 0; i < count; i++) {
-            meter = meter + step;
-            answer.push_back(meter);
+          meter = meter + step;
+          answer.push_back(meter);
         }
     }
     return answer;
